bd90bab689df1952cb774e1697b2db838f9a6f1b
leninred <haker080@mail.ru>
2018-11-22 18:25:35 +0500

    Cleaning up project

--- bestAPPever/Form1.cs
+++ bestAPPever/Form1.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace bestAPPever
 {
@@ -15,21 +7,6 @@ namespace bestAPPever
         public Form1()
         {
             InitializeComponent();
-        }
-
-        //Комент ёобана
-        bool a = true;
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (a)
-            {
-                label1.Text = "Привет Илья, я кодер Саша";
-                a = false;
-            }
-            else
-            {
-                label1.Text = "Зачем нажимать дважды а?!";
-            }
         }
     }
 }
